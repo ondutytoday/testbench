cae25faa3610f7e95f6e320cdcd8894daf221e89
Elena Vasileva <elenavw@gmail.com>
2020-06-29 20:59:09 +0400

    Commit 11

--- src/test/java/ru/vasileva/benchbackend/view/ConsoleHelperTest.java
+++ src/test/java/ru/vasileva/benchbackend/view/ConsoleHelperTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class ConsoleHelperTest {
 
     @Test
-    void readString() {
+    void readSimpleString() {
+        assertEquals("test", "test");
     }
+
 }
